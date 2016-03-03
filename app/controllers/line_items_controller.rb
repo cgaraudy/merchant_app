e5ab57a899b1647842f2adee84c4609498d7026c
cgaraudy <cgaraudy@uno.edu>
2016-03-03 14:48:56 +0000

    Fixes multiple bugs in shopping cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -4,11 +4,11 @@ class LineItemsController < ApplicationController
     
     def create
         product = Product.find(params[:product_id])
-        @line_item = @cart.line_items.build(product: product, price: product.current_price)
+        @line_item = @cart.add_product(product.id) 
         
         if @line_item.save
             flash[:notice] = "#{product.name} was successfully added to your cart!"
-            redirect_to shop_path
+            redirect_to shop_url
         else
             flash.now[:alert] = "Could not add item to your cart."
             redirect_to :back
