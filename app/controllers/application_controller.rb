c1c2cb59948d1dfc1b1fde5f88268c3496c1f419
cgaraudy <cgaraudy@uno.edu>
2016-03-09 15:50:41 +0000

    adds admin stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+  
+  def require_admin_user
+    unless user_signed_in? && current_user.admin
+      redirect_to root_url, alert: "You shall not pass."
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160303145330) do
+ActiveRecord::Schema.define(version: 20160309154055) do
 
   create_table "brands", force: :cascade do |t|
     t.string   "name"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20160303145330) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
